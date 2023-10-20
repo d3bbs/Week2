319c496f09ae3f23d0bb2bf83a0497f1ab05f826
d3bbs <aaron.debattista.f31966@mcast.edu.mt>
2023-10-20 09:42:39 +0200

    first commit

---
+++ week2.py
@@ -0,0 +1,31 @@
+a= input("Enter a number: ")
+a= int(a)
+
+if (a>1):
+    print("A is larger than 1")
+    
+elif (a==1):
+    print("A is 1")
+
+else:
+    print("A is less than 1")
+    
+    
+    
+    
+    
+    
+grade= int(input("Enter a grade "))
+if (grade>=90)and (grade<=100):
+    print("You got an A+")
+elif (grade>=80) and (grade<=89):
+    print("You got an A")
+elif (grade>=70) and (grade<=79):
+    print("You got an B")
+elif (grade>=60) and (grade<=69):
+    print("You got an C")
+elif (grade>=50) and (grade<=59):
+    print("You got an D")
+elif (grade<=49) and (grade>=0):
+    print("You got an F")    
+
